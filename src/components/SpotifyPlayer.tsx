c002ecad94838ec41e6e294647463203a01a80de
arhcve <paulanandvr@gmail.com>
2026-08-17 22:52:00 +0530

    Restore SpotifyPlayer to pre-August 17 version

--- src/components/SpotifyPlayer.tsx
+++ src/components/SpotifyPlayer.tsx
@@ -1,4 +1,20 @@
-import { useCallback, useEffect, useRef, useState } from "react";
+import { SPOTIFY_PLAYLIST_ID } from "@/data/places";
+
+export function SpotifyPlayer() {
+  return (
+    <div className="spotify-shell">
+      <iframe
+        title="Spotify Playlist"
+        src={`https://open.spotify.com/embed/playlist/${SPOTIFY_PLAYLIST_ID}?utm_source=generator&theme=0`}
+        width="100%"
+        height="152"
+        frameBorder={0}
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+        loading="lazy"
+      />
+    </div>
+  );
+}import { useCallback, useEffect, useRef, useState } from "react";
 import { PLAYLIST_TRACKS } from "@/data/playlist";
 
 type EmbedController = {
