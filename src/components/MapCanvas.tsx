60024e11ea4008e5ffd6a4fec9f3f4551d63d3e6
arhcve <paulanandvr@gmail.com>
2026-08-18 11:48:21 +0530

    Add architectural graphic map layer

--- src/components/MapCanvas.tsx
+++ src/components/MapCanvas.tsx
@@ -33,6 +33,23 @@ export default function MapCanvas() {
     const centre = bounds.getCenter();
 
     for (const place of PLACES) {
+      const frameOffset = 0.00016;
+      L.rectangle(
+        [
+          [place.lat - frameOffset, place.lng - frameOffset],
+          [place.lat + frameOffset, place.lng + frameOffset],
+        ],
+        {
+          color: CYAN,
+          weight: 0.75,
+          opacity: 0.5,
+          fillColor: CYAN,
+          fillOpacity: 0.035,
+          interactive: false,
+          className: "site-frame",
+        },
+      ).addTo(map);
+
       const marker = L.circleMarker([place.lat, place.lng], {
         radius: 7,
         color: CYAN,
@@ -71,5 +88,29 @@ export default function MapCanvas() {
     };
   }, []);
 
-  return <div ref={containerRef} className="aerial-map absolute inset-0 h-full w-full" />;
+  return (
+    <div className="map-stage absolute inset-0">
+      <div ref={containerRef} className="aerial-map absolute inset-0 h-full w-full" />
+      <svg
+        className="topographic-overlay"
+        viewBox="0 0 1000 700"
+        preserveAspectRatio="none"
+        aria-hidden="true"
+      >
+        <g className="contour-lines">
+          <path d="M-80 76 C75 18 176 45 243 112 S338 211 443 157" />
+          <path d="M-90 108 C64 49 165 75 228 140 S322 237 426 188" />
+          <path d="M-96 143 C53 82 151 106 211 167 S305 260 407 220" />
+          <path d="M653 525 C722 438 820 426 907 477 S1023 556 1092 487" />
+          <path d="M632 562 C710 468 816 456 913 510 S1030 589 1096 526" />
+          <path d="M610 603 C698 501 812 491 918 546 S1038 626 1105 568" />
+          <path d="M-65 590 C61 519 154 530 219 595 S330 690 445 638" />
+        </g>
+        <g className="survey-ticks">
+          <path d="M28 64h42M49 43v42M930 78h42M951 57v42" />
+          <path d="M28 622h42M49 601v42M930 622h42M951 601v42" />
+        </g>
+      </svg>
+    </div>
+  );
 }
